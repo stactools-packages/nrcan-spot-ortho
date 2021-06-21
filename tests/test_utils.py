b2393922c894806584998a70caa5537ad7516808
James Townend <jvrt@pm.me>
2021-06-21 10:06:28 -0700

    fixed formatting

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,20 +3,24 @@ import fiona
 
 # Test cases, file names to keys and values that should exist.
 schema = {
-    'properties': OrderedDict([('NAME', 'str:50')]), 
-    'geometry': 'Polygon'}
+    'properties': OrderedDict([('NAME', 'str:50')]),
+    'geometry': 'Polygon'
+}
 
 src0 = {
     'type': 'Feature',
     'id': '0',
     'properties': OrderedDict([('NAME', 'S5_09537_5435_20070531')]),
-    'geometry': {'type': 'Polygon',
-    'coordinates': [[
-        (-78528.00240865147, 598573.004153735),
-        (-58279.99927707571, 653731.9967474313),
-        (-181.00179570007555, 632649.0025307061),
-        (-20390.002998122636, 577274.0012620063),
-        (-78528.00240865147, 598573.004153735)]]}}
+    'geometry': {
+        'type':
+        'Polygon',
+        'coordinates': [[(-78528.00240865147, 598573.004153735),
+                         (-58279.99927707571, 653731.9967474313),
+                         (-181.00179570007555, 632649.0025307061),
+                         (-20390.002998122636, 577274.0012620063),
+                         (-78528.00240865147, 598573.004153735)]]
+    }
+}
 
 crs = {
     'proj': 'lcc',
@@ -28,11 +32,15 @@ crs = {
     'y_0': 0,
     'datum': 'NAD83',
     'units': 'm',
-    'no_defs': True}
+    'no_defs': True
+}
+
 
 def write_test_index(test_index_path):
 
-    with fiona.open(
-        test_index_path, mode='w', driver='ESRI Shapefile', schema=schema, crs=crs
-        ) as output:
+    with fiona.open(test_index_path,
+                    mode='w',
+                    driver='ESRI Shapefile',
+                    schema=schema,
+                    crs=crs) as output:
         output.write(src0)
